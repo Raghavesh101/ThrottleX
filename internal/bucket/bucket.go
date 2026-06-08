1fde4e9d5a9ca0be36fcdea9d0f42fb543069e01
Raghavesh <raghaveshm@gmail.com>
2026-06-08 22:57:26 +0530

    Allow function

--- internal/bucket/bucket.go
+++ internal/bucket/bucket.go
@@ -10,3 +10,23 @@ type Bucket struct {
 	tokens     float64
 	lastRefill time.Time
 }
+
+func NewBucket(capacity, refillRate float64) *Bucket {
+	return &Bucket{
+		capacity:   capacity,
+		refillRate: refillRate,
+		tokens:     capacity,
+		lastRefill: time.Now(),
+	}
+}
+
+func (b *Bucket) Allow() bool {
+	now := time.Now()
+	elapsed := now.Sub(b.lastRefill).Seconds()
+	b.tokens = min(b.tokens+elapsed*b.refillRate, b.capacity)
+	if b.tokens >= 1 {
+		b.tokens--
+		return true
+	}
+	return false
+}
